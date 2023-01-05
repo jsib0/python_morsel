2114ff5bd6a546ef6f6d5c9881eaeb543572cc9c
jonsibala <sibalajp@gmail.com>
2023-01-04 23:13:28 -0800

    WIP. practicing enums, the difference between using methods in a class vs. using individual functions

---
+++ exercises/diving_into_classes/nextdate.py
@@ -0,0 +1,34 @@
+from datetime import date, datetime
+from enum import IntEnum, auto
+
+
+class Weekday(IntEnum):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
+
+
+class NextDate:
+    """Given the next weekday, calculates the days until the given day."""
+
+    def __init__(self, next_weekday):
+        self.next_weekday = next_weekday
+
+    def date(self):
+        """Returns next weekday's date"""
+        today = date.today()
+        return today + timedelta((self.next_weekday - today.weekday()) % 7)
+
+    def days_until(self):
+        """days until given next weekday"""
+        return (self.date() - date.today()).days
+
+
+
+
+
+
